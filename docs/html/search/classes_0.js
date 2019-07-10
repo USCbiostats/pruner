c90c6981811ebd1097e2dee318bb0ce18983e981
George G. Vega Yon <g.vegayon@gmail.com>
2019-07-10 14:30:13 -0700

    Fixing travis (adding the docs/ folder for doxygen)

---
+++ docs/html/search/classes_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['tree',['Tree',['../classpruner_1_1Tree.html',1,'pruner']]],
+  ['tree',['Tree',['../classTree.html',1,'']]],
+  ['treeiterator',['TreeIterator',['../classTreeIterator.html',1,'']]],
+  ['treeiterator',['TreeIterator',['../classpruner_1_1TreeIterator.html',1,'pruner']]]
+];
