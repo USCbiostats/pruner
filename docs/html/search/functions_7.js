34c7b22bfd7985284b177c75711276c2b615d32b
George G. Vega Yon <g.vegayon@gmail.com>
2019-07-21 13:48:48 -0700

    Fixing bug in reseting pruning

---
+++ docs/html/search/functions_7.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['up',['up',['../classpruner_1_1TreeIterator.html#adca1d999f093a69e2f5d044b358e5da7',1,'pruner::TreeIterator::up()'],['../classTreeIterator.html#adca1d999f093a69e2f5d044b358e5da7',1,'TreeIterator::up()']]]
+];
